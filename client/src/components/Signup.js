e20003fd056e88744846f14644451faf7715fd0f
Vishal Kondle <vishalkondle@gmail.com>
2023-02-13 21:31:57 +0530

    adding signup page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,52 @@
-import React from "react";
+import { TextInput, Button, Group, Box, PasswordInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
-const Signup = () => {
-  return <div>Signup</div>;
-};
+function Signup() {
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      name: (value) => (value?.length > 6 ? null : "Atlease 6 characters"),
+      password: (value) => (value?.length > 8 ? null : "Atlease 6 characters"),
+    },
+  });
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+        <TextInput
+          withAsterisk
+          label="Name"
+          placeholder="Name"
+          {...form.getInputProps("name")}
+        />
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps("email")}
+        />
+        <PasswordInput
+          withAsterisk
+          label="Password"
+          placeholder="Password"
+          {...form.getInputProps("password")}
+        />
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
+  );
+}
 
 export default Signup;
