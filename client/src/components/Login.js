274b7594bfe15fc56499cfe1025e74b485503e4c
Vishal Kondle <vishalkondle@gmail.com>
2023-02-13 22:31:23 +0530

    Creating Login Page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,53 @@
-import React from "react";
+import { TextInput, Button, Group, Box, PasswordInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
-  return <div>Login</div>;
-};
+function Login() {
+  const history = useNavigate();
+
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
+      password: (value) => (value?.length > 8 ? null : "Atlease 6 characters"),
+    },
+  });
+
+  const handleSubmit = async (values) => {
+    const { data } = await axios.post(
+      "http://localhost:5000/api/login",
+      values
+    );
+    console.log(data);
+    history("/user");
+  };
+
+  return (
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="your@email.com"
+          {...form.getInputProps("email")}
+        />
+        <PasswordInput
+          withAsterisk
+          label="Password"
+          placeholder="Password"
+          {...form.getInputProps("password")}
+        />
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      </form>
+    </Box>
+  );
+}
 
 export default Login;
