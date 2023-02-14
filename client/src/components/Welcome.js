902b7cb45f623c6eded471d4e5b71bf32cf020e7
Vishal Kondle <vishalkondle@gmail.com>
2023-02-14 12:24:22 +0530

    Creating welcome page and sending api call with wihCredentials

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,7 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+axios.defaults.withCredentials = true;
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  const [user, setUser] = useState();
+  const getUser = async () => {
+    const { data } = await axios
+      .get("http://localhost:5000/api/user", {
+        withCredentials: true,
+      })
+      .catch((error) => console.log(error));
+    return data;
+  };
+
+  useEffect(() => {
+    getUser().then((res) => setUser(res.user));
+  }, []);
+
+  return <div>{user && <h1>Welcome {user.name}</h1>}</div>;
 };
 
 export default Welcome;
